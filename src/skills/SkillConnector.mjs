b56b62896b64b36e51b4c63dd31686c0f327f6f6
Orfeas Artinopoulos <artinopoulos.orfeas@gmail.com>
2025-04-27 16:37:40 +0300

    FIX Refactor SkillConnector to streamline patch management and standardize logging ADD Base game patches for skill unlock and relic unlock on the respective gamemodes ADD Into the Abyss expansion abyssal level up patch

--- src/skills/SkillConnector.mjs
+++ src/skills/SkillConnector.mjs
@@ -6,10 +6,11 @@ export class SkillConnector {
         throw new Error(`[PSY] ${this.constructor.name} class is abstract and cannot be initialized directly.`);
       }
 
-    this.audioMng = null; // Initialize with a default value
-    this.skillname = null; // Initialize with a default value
-    this.skill = null; // Initialize with a default value
-    this.patchAddXP = null;
+    this.audioMng = null;
+    this.skillname = null;
+    this.skill = null;
+
+    this.patchMap = new Map();
     }
 
     // Getter for audio manager
@@ -42,19 +43,12 @@ export class SkillConnector {
         this.skill = skill;
     }
 
-    // Getter for patchAddXP
-    get patchAddXPInstance() {
-        return this.patchAddXP;
-    }
-
-    // Setter for patchAddXP
-    set patchAddXPInstance(patch) {
-        this.patchAddXP = patch;
+    // Getter for patchMap
+    get patchMapInstance() {
+        return this.patchMap;
     }
 
     initializeConnector() {
-        // TODO Figure out the logic | Patches should be performed here ?
-
         // Base game patches
         this.initBaseGamePatches();
 
@@ -76,72 +70,88 @@ export class SkillConnector {
     // Game & expansion patches
     initBaseGamePatches()
     {
-        console.log("Patching base game functions...");
+        console.log("[PSY] Ear Candy | Patching base game functions...");
 
-        this.patchXpDrop();
         this.patchActionInterval();
+        this.patchXpDrop();
         this.patchLevelUp();
+
+        if(game.currentGamemode.localID == 'Hardcore' ||
+            game.currentGamemode.localID == 'AncientRelics')
+            {
+                this.patchOnUnlockSkill();
+            }
+
+        if(game.currentGamemode.localID == 'AncientRelics')
+            {
+                this.patchAncientRelicUnlock();
+            }
     }
 
     initTotHPatches()
     {
-        console.log("Throne of the Herald expansion detected, patching functions...");
+        console.log("[PSY] Ear Candy | Throne of the Herald expansion detected, patching functions...");
     }
 
     initAoDPatches()
     {
-        console.log("Atlas of Discovery expansion detected, patching functions...");
+        console.log("[PSY] Ear Candy | Atlas of Discovery expansion detected, patching functions...");
     }
 
     initItAPatches()
     {
-        console.log("Into the Abyss expansion detected, patching functions...");
+        console.log("[PSY] Ear Candy | Into the Abyss expansion detected, patching functions...");
         this.patchAbyssalLevelUp();
     }
     // Game & expansion patches end
 
-
-    patchXpDrop() {
-        // TODO Patch XP drop
-        let sAddXP = getResourceUrl(`assets/sfx/${this.skillName}/addXP.wav`);
-        this.patchAddXPInstance = patch(this.skillInstance.constructor, 'addXP');
-        // this.patchAddXPInstance.after(this.afterXpDropPatch());
-        this.patchAddXPInstance.after(() => { this.audioManager.playActionSound(this.skillName, getResourceUrl(`assets/sfx/${this.skillName}/addXP.wav`))});
-        console.log(`[${this.constructor.name}] Patching XP drop...`);
-    }
-
+    // Base game patches
     patchActionInterval() {
         // TODO Patch action interval
+        // May be used for specific skills only
         console.log(`[${this.constructor.name}] Patching action interval...`);
     }
 
-    patchLevelUp() {
-        // TODO Patch level up
-        console.log(`[${this.constructor.name}] Patching level up...`);
+    patchXpDrop() {
+        this.patchMapInstance.set('addXP', patch(this.skillInstance.constructor, 'addXP'));
+        this.patchMapInstance.get('addXP').after(() => { this.audioManager.playActionSound(this.skillName, getResourceUrl(`assets/sfx/Skills/${this.skillName}/addXP.wav`))});
+        console.log(`[${this.constructor.name}] Patching XP drop...`);
     }
 
-    patchAbyssalLevelUp() {
-        // TODO Patch abyssal level up
-        console.log(`[${this.constructor.name}] Patching abyssal level up...`);
+    patchLevelUp() {
+        this.patchMapInstance.set('levelUp', patch(this.skillInstance.constructor, 'levelUp'));
+        this.patchMapInstance.get('levelUp').after(() => { this.audioManager.playActionSound(this.skillName, getResourceUrl(`assets/sfx/Skills/${this.skillName}/levelUp.ogg`))});
+        console.log(`[${this.constructor.name}] Patching level up...`);
     }
 
     // if skill is locked
     patchOnUnlockSkill()
     {
-        // TODO Patch on unlock skill
+        this.patchMapInstance.set('onUnlock', patch(this.skillInstance.constructor, 'onUnlock'));
+        this.patchMapInstance.get('onUnlock').after(() => { this.audioManager.playActionSound(this.skillName, getResourceUrl(`assets/sfx/Misc/Skills/onUnlock.ogg`))});
         console.log(`[${this.constructor.name}] Patching on unlock skill...`);
     }
 
     // If character mode is ancient relic
     patchAncientRelicUnlock()
     {
-        // TODO Patch ancient relic unlock
+        this.patchMapInstance.set('onAncientRelicUnlock', patch(this.skillInstance.constructor, 'onAncientRelicUnlock'));
+        this.patchMapInstance.get('onAncientRelicUnlock').after(() => { this.audioManager.playActionSound(this.skillName, getResourceUrl(`assets/sfx/Misc/Skills/onAncientRelicUnlock.ogg`))});
         console.log(`[${this.constructor.name}] Patching ancient relic unlock...`);
     }
+    // Base game patches end
 
+    // Into the Abyss expansion patches
+    patchAbyssalLevelUp() {
+        this.patchMapInstance.set('abyssalLevelUp', patch(this.skillInstance.constructor, 'abyssalLevelUp'));
+        this.patchMapInstance.get('abyssalLevelUp').after(() => { this.audioManager.playActionSound(this.skillName, getResourceUrl(`assets/sfx/Skills/${this.skillName}/abyssalLevelUp.ogg`))});
+        console.log(`[${this.constructor.name}] Patching abyssal level up...`);
+    }
+    // Into the Abyss expansion patches end
+
+    //TODO Test function - remove once done
     helloWorld() {
         console.log(`[${this.constructor.name}] Hello world!`);
-        // this.audioMng.play(getResourceUrl('assets/sfx/Woodcutting/addXP.wav'));
     }
 
   }
